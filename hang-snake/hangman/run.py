c6115e7b022afa265041498941d7d5b26db28e93
pocht <andrejtimofeev311@gmail.com>
2023-09-15 19:02:56 +0300

    Виселица с двумя языками, но без генерации слова.

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,11 +1,41 @@
 from common.util import clear_terminal
 
+#Генератор слова:
 
-def create_secret():
+
+def create_secret_eng():
     return 'capybara'
 
 
-SECRET = create_secret()
+def create_secret_rus():
+    return 'капибара'
+
+
+#Выбор языка:
+while True:
+    language = input('Select language/Выберите язык (Eng/Rus): ')
+    if language == 'Eng':
+        SECRET = create_secret_eng()
+        lett_choosing = 'Enter your letter: '
+        invalid_letter = 'Invalid input! Try again'
+        wrong_letter = "This letter isn't in the word"
+        losing = 'GAME OVER!'
+        winning = 'You won! Congratulations!'
+
+        break
+    elif language == 'Rus':
+        SECRET = create_secret_rus()
+        lett_choosing = 'Введите букву: '
+        invalid_letter = 'Некорректный ввод. Попробуйте еще раз'
+        wrong_letter = 'Этой буквы нет в слове'
+        losing = 'ПОТРАЧЕНО.'
+        winning = 'Победа!'
+
+        break
+    else:
+        print('Invalid input! Try again/Некорректный ввод. Попробуйте еще раз')
+
+
 secret_letters = ['_'] * len(SECRET)
 
 FIELDS = [
@@ -90,11 +120,13 @@ while True:
     print(''.join(secret_letters))
     print(FIELDS[turn_number])
 
-    letter = input('Enter your guess: ').lower()
+    letter = input(lett_choosing).lower()
 
     #2
-    if (len(letter)) != 1 or ord(letter) < ord('a') or ord(letter) > ord('z'):
-        print('Invalid input! Try again')
+    if (len(letter)) != 1 or (language == 'Eng' and (ord(letter) < ord('a') or ord(letter) > ord('z'))) or \
+            (language == 'Rud' and (ord(letter) < ord('а') or ord(letter) > ord('я'))):
+
+        print(invalid_letter)
         continue
     #3
     if letter in SECRET:
@@ -102,15 +134,15 @@ while True:
             if SECRET[i] == letter:
                 secret_letters[i] = letter
     else:
-        print('Wrong letter')
+        print(wrong_letter)
         turn_number += 1
 
     #5
     if turn_number == len(FIELDS) - 1:
         print(FIELDS[turn_number])
-        print('GAME OVER!!!')
+        print(losing)
         break
     if ''.join(secret_letters) == SECRET:
-        print('You won!')
+        print(winning)
         print(SECRET.upper())
         break
