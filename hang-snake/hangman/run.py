f98be204fb6fea8af11d70bbf7fa1620bd283763
Maxim Golchanskiy <golchanskiy.m.d@gmail.com>
2023-09-16 17:14:20 +0300

    Hangman game realization

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,33 +1,93 @@
 from common.util import clear_terminal
-
-
+class StaticVar:
+    START_NUM = 0
 def create_secret():
-    return 'capybara'
-
+        return 'capybara'
 
 SECRET = create_secret()
 n = len(SECRET)
 
-FINAL_FIELD = r'''
-   +----+
-   |    |
-   o    |
-  /|\   |
-  / \   |
-_______/|\_
-'''
-
 # здесь мы наверное хотим иметь исходное поле
 # и понимание, как оно меняется после каждого хода
-FIELD = FINAL_FIELD
 
-while True:
-    # make a move!
-    letter = input('Enter your guess: ')
-    if ...:
-        FIELD = ...  # если не угадали, то надо обновить поле
+ZERO_STATE = r''''''
+FIRST_STATE = r'''
+
+
+
+
+
+    _______/|\_
+    '''
+SECOND_STATE = r'''
+
+            |
+            |
+            |
+            |
+    _______/|\_
+    '''
+THIRD_STATE = r'''
+       +----+
+            |
+            |
+            |
+            |
+    _______/|\_
+    '''
+FORTH_STATE = r'''
+       +----+
+       |    |
+            |
+            |
+            |
+    _______/|\_
+    '''
+FINAL_STATE = r'''
+       +----+
+       |    |
+       o    |
+      /|\   |
+      / \   |
+    _______/|\_
+    '''
+
+state = [ZERO_STATE, FIRST_STATE, SECOND_STATE, THIRD_STATE, FORTH_STATE, FINAL_STATE]
+
+# Создаём мапу из исходного слова и добавляем в неё каждую букву строки
+dict = dict()
+def fillDictionary(string, dict):
+    for i in SECRET:
+        if i not in dict:
+            dict[i] = 1
+        else:
+            dict[i] += 1
+
+
+def logic(letter, dict_):
+    if letter in dict_:
+        dict_.pop(letter)
     else:
-        ...  # мало ли, понадобится...
+        StaticVar.START_NUM += 1
+    returnCurrState()
+    if StaticVar.START_NUM == 5:
+        exit(1)
+    if len(dict_) == 0:
+        print("YOU WIN!!!")
+        exit(0)
 
-    clear_terminal()
-    print(FIELD)
+
+def returnCurrState():
+    print(state[StaticVar.START_NUM])
+
+    # while True:
+    # make a move!
+    # letter = input('Enter your guess: ')
+    # f ...:
+    #    FIELD = ...  # если не угадали, то надо обновить поле
+    # else:
+    #    ...  # мало ли, понадобится...
+
+    #clear_terminal()
+    #print(FIELD)
+
