1864cc5113f08727c9ff149bf8791d1cbdfb374c
PYnix <yana5nizkaya@ya.ru>
2023-09-19 22:43:43 +0300

    первая работающая версия

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,33 +1,106 @@
 from common.util import clear_terminal
+import random
 
+def field(a):
+
+    f0 = r'''
+       +----+
+       |    |
+       o    |
+      /|\   |
+      / \   |
+    _______/|\_
+    '''
+    f1 = r'''
+       +----+
+       |    |
+       o    |
+      /|\   |
+            |
+    _______/|\_
+    '''
+    f2 = r'''
+       +----+
+       |    |
+       o    |
+            |
+            |
+    _______/|\_
+    '''
+    f3 = r'''
+       +----+
+       |    |
+            |
+            |
+            |
+    _______/|\_
+    '''
+    f4 = r'''
+       +----+
+            |
+            |
+            |
+            |
+    _______/|\_
+    '''
+    f5 = r'''
+            +
+            |
+            |
+            |
+            |
+    _______/|\_
+    '''
+    f6 = r'''
+            
+            
+            
+            
+            
+    _______/|\_
+    '''
+    f7 = " "
+    arr=[f0, f1, f2, f3, f4, f5, f6, f7]
+    return arr[a]
 
 def create_secret():
-    return 'capybara'
+    dict = [i for i in open("dictionary")]
+    i = random.randint(0, len(dict))
+    return dict[i]
 
 
 SECRET = create_secret()
-n = len(SECRET)
+WORD = ['_' for i in range(len(SECRET)-1)]
 
-FINAL_FIELD = r'''
-   +----+
-   |    |
-   o    |
-  /|\   |
-  / \   |
-_______/|\_
-'''
-
-# здесь мы наверное хотим иметь исходное поле
-# и понимание, как оно меняется после каждого хода
-FIELD = FINAL_FIELD
-
-while True:
+already_named = set()
+TRIES = 7
+FIELD = field(TRIES)
+print(*WORD)
+while TRIES>0:
     # make a move!
     letter = input('Enter your guess: ')
-    if ...:
-        FIELD = ...  # если не угадали, то надо обновить поле
+    if letter not in SECRET:
+        TRIES -= 1
+        already_named.add(letter)
+        FIELD = field(TRIES)  # если не угадали, то надо обновить поле
+
     else:
-        ...  # мало ли, понадобится...
+        unknown_let = 0
+        for i in range(len(SECRET)-1):
+            if SECRET[i]==letter:
+                WORD[i]=letter
+            if WORD[i]=='_':
+                unknown_let+=1
+
+        if unknown_let==0:
+            print(*WORD)
+            print("YOU WIN!!")
+            break
+    print("не подошли: ", *already_named)
+    print(*WORD)
 
     clear_terminal()
     print(FIELD)
+if TRIES==0:
+    print(SECRET)
+    print("YOU'VE LOST\n"+"DON'T WORRY, TRY AGAIN!")
