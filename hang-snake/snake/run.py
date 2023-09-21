35c5e53e653016c393a631e919a351b07cbbd988
lizavetashap <liza-shap@yandex.ru>
2023-09-22 00:36:38 +0300

    Snake - demo иногда ломается и растет бесконечно, не убивается об себя (в коде закомменчена возможность, не знаю будет ли она работать). из приятного - ест яблоко, убивается об стенки, чаще всего нормально растет и управляется

--- hang-snake/snake/run.py
+++ hang-snake/snake/run.py
@@ -1,36 +1,89 @@
 from pynput import keyboard
+import time
+import os
 from random import randint
 
-WIDTH, HEIGHT = ..., ...
-# можно приделать конфиг-файл с параметрами
-direction = (1, 0)
-
-
+#генерируем позицию яблока
 def random_position():
-    return randint(0, HEIGHT - 1), randint(0, WIDTH - 1)
+    row = randint(1, HEIGHT - 2)
+    col = randint(1, WIDTH - 2)
+    while (col, row) in snake: #проверяем не в змею ли сгенерировалось
+        row = randint(1, HEIGHT - 2)
+        col = randint(1, WIDTH - 2)
+    return(col, row)
 
+#наше поле
+def print_field():
+    for cell in CELLS:
+        if cell in snake:
+            print('s', end = '')
+        elif cell[0] in (0, WIDTH - 1):
+            print('|', end = '')
+        elif cell[1] in (0, HEIGHT - 1):
+            print('_', end = '')
+        elif cell == apple:
+            print('a', end = '')
+        else:
+            print(' ', end = '')
 
+        if cell[0] == WIDTH -1:
+            print('')
+
+#змейка ползает
+def snake_move():
+    global eat
+    head_snake = snake[0][0] + direction[0], snake[0][1] + direction[1] #делаем змейке новую голову, чтобы она ползла вперед
+    snake.insert(0, head_snake)
+    if not eat: #если змея съела яблоко то обновляем переменную
+        snake.pop(-1)
+        eat = False
+
+#механизм поедания яблока
+def apple_random():
+    global apple, eat
+    if apple == snake[0]:
+        apple = random_position()
+        eat = True
+
+WIDTH, HEIGHT = 30, 15 #параметры поля
+CELLS = [(col, row) for row in range(HEIGHT) for col in range(WIDTH)]
+
+snake = [(5, HEIGHT // 2), (4, HEIGHT // 2), (3, HEIGHT // 2)] #генерируем змею на поле
+apple = random_position() #генерируем яблоко на поле
+eat = False
+
+#управление
 def process_press(key):
     # обработчик нажатия на клавиши (можно сделать и поаккуратнее)
     global direction
     match key:
         case keyboard.Key.left:
-            direction = (0, -1)
-        case keyboard.Key.up:
             direction = (-1, 0)
+        case keyboard.Key.up:
+            direction = (0, -1)
         case keyboard.Key.right:
-            direction = (0, 1)
-        case keyboard.Key.down:
             direction = (1, 0)
+        case keyboard.Key.down:
+            direction = (0, 1)
 
 
-snake = [random_position()]
-Apple = random_position()
-while apple in snake:
-    apple = random_position()
+direction = (1, 0)
 
-# оно умеет мониторить нажатия на кнопки!
+#делаем больше одного хода
 with keyboard.Listener(on_press=process_press) as listener:
     while True:
-        # let's play the game!
-        pass
+        os.system('cls')
+        print_field()
+        snake_move()
+        apple_random()
+
+
+        if snake[0][0] in (0, WIDTH) or\
+            snake[0][1] in (0, HEIGHT):
+        #or snake[0] in snake[1:] не уверена будет ли это работать
+            os.system('cls')
+            print('Game Over')
+            break
+
+        time.sleep(0.5)
+
