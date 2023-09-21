8feb459aae12888bb4f0604e255e849a7713280b
ArtemSpirin <54719620+ArtemSpirin@users.noreply.github.com>
2023-09-22 01:45:53 +0300

    Update run.py

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,33 +1,35 @@
-from common.util import clear_terminal
-
-
+from util import clear_terminal
+from words_and_alphabet import *
+import random
+from parts_of_image import *
 def create_secret():
-    return 'capybara'
-
-
+    return random.choice(ListOfWords)
 SECRET = create_secret()
 n = len(SECRET)
+tries=len(field)
+tries_left =len(field)
+letters =["_ "]*n
+while tries_left>0:
+    in_or_not = 0
+    print(field[tries_lefts-tries_left])
+    print(''.join(letters))
+    print('Enter your latter:')
+    ans = input()
+    if ans not in alphabet:
+        try:
+            x = int('a')
+        except ValueError:
+                in_or_not = 1
+                error =  1
+                print("Oops!  That was no valid letter.  Try again...")
+    for i in range(len(SECRET)):
+        if ans==SECRET[i]:
+                letters[i]=ans
+                in_or_not =1
+    if "_ " not in letters and tries_left>0:
+        print("WIN!!!")
+        break
+    if in_or_not == 0:
+        tries_left-=1
+clear_terminal()
 
-FINAL_FIELD = r'''
-   +----+
-   |    |
-   o    |
-  /|\   |
-  / \   |
-_______/|\_
-'''
-
-# здесь мы наверное хотим иметь исходное поле
-# и понимание, как оно меняется после каждого хода
-FIELD = FINAL_FIELD
-
-while True:
-    # make a move!
-    letter = input('Enter your guess: ')
-    if ...:
-        FIELD = ...  # если не угадали, то надо обновить поле
-    else:
-        ...  # мало ли, понадобится...
-
-    clear_terminal()
-    print(FIELD)
