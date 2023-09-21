9947b14ac843e21f3b9c3e93b208f1e5ece544a2
lizavetashap <liza-shap@yandex.ru>
2023-09-21 16:56:28 +0300

    Hangman - done

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,33 +1,131 @@
 from common.util import clear_terminal
 
 
+
 def create_secret():
-    return 'capybara'
+    return 'солнышко'
+word = create_secret()
 
+FIELD = (
+    """
+              
+              
+              
+              
+              
+     _______/|\_ 
+    """,
+    """
+              
+              
+              
+             |
+             |
+     _______/|\_ 
+    """,
+    """
+              
+             |
+             |
+             |
+             |
+     _______/|\_ 
+    """,
+    """
+       +-----+
+             |
+             |
+             |
+             |
+     _______/|\_ 
+    """,
+    """
+       +-----+
+       |     |
+             |
+             |
+             |
+     _______/|\_ 
+    """,
+    """
+       +-----+ 
+       |     |
+       o     |
+             |
+             |
+     _______/|\_ 
+    """,
+    """
+       +-----+
+       |     |
+       o     |
+       |     |
+             |
+     _______/|\_ 
+    """,
+    """
+       +-----+
+       |     |
+       o     |
+      /|     |
+             |
+     _______/|\_ 
+    """,
+    """
+       +-----+
+       |     |
+       o     |
+      /|\    |
+             |
+     _______/|\_ 
+    """,
+    """
+       +-----+
+       |     |
+       o     |
+      /|\    |
+      /      |
+     _______/|\_ 
+    """,
+    """
+       +-----+
+       |     |
+       o     |
+      /|\    |
+      / \    |
+     _______/|\_ 
+    """
+)
+number_of_errors = 0 #кол-во ошибок сделанных игроком
+maximum_erorrs = 10 #максимальное количество ошибок
+n = len(word)
+guessed = ['_' for _ in range(n)] #угаданные
+print(''.join(guessed))
 
-SECRET = create_secret()
-n = len(SECRET)
-
-FINAL_FIELD = r'''
-   +----+
-   |    |
-   o    |
-  /|\   |
-  / \   |
-_______/|\_
-'''
-
-# здесь мы наверное хотим иметь исходное поле
-# и понимание, как оно меняется после каждого хода
-FIELD = FINAL_FIELD
-
-while True:
-    # make a move!
-    letter = input('Enter your guess: ')
-    if ...:
-        FIELD = ...  # если не угадали, то надо обновить поле
+while guessed != word and number_of_errors < maximum_erorrs:
+    print('Enter your guess: ')# make a move!
+    letter = input() #игрок вводит букву
+    if letter in word:
+        new = ""
+        for i in range(len(word)): #ищем место буквы в слове
+            if letter == word[i]:
+                new += letter
+            else:
+                new += guessed[i]
+        guessed = new
+        print(''.join(guessed))
+        print(FIELD[number_of_errors])
     else:
-        ...  # мало ли, понадобится...
+        print(('There is no such letter in word.'))
+        number_of_errors +=1
+        print(''.join(guessed))
+        print(FIELD[number_of_errors])
+
+if number_of_errors == maximum_erorrs:
+    print(FIELD[number_of_errors])
+    print("Game over")
+else:
+    print("Congratulations!The word was", word)
 
     clear_terminal()
-    print(FIELD)
+
