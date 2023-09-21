60b2ed0166d9547554d576ed8f6935ae67ed95a5
100yef <100yef@gmail.com>
2023-09-21 17:14:17 +0300

    Added list of words, added answer field, added all hangman cases

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,14 +1,62 @@
+import random
+
 from common.util import clear_terminal
+
+words = ['programming', 'python', 'laptop', 'coffee']  # Список слов
 
 
 def create_secret():
-    return 'capybara'
+    return random.choice(words)  # Выбор случайного слова из списка
 
-#sample text
+
 SECRET = create_secret()
 n = len(SECRET)
+guess_field = '_' * n  # Создание слова и поля для ответа
 
-FINAL_FIELD = r'''
+mistakes = 0  # Счет количества ошибок, от которого зависит какое поле выведено на экран
+
+fields = [
+    r'''
+   +----+
+   |    |
+        |
+        |
+        |
+_______/|\_
+''',
+    r'''
+   +----+
+   |    |
+   o    |
+        |
+        |
+_______/|\_
+''',
+    r'''
+   +----+
+   |    |
+   o    |
+   |    |
+        |
+_______/|\_
+''',
+    r'''
+   +----+
+   |    |
+   o    |
+  /|\   |
+        |
+_______/|\_
+''',
+    r'''
+   +----+
+   |    |
+   o    |
+  /|\   |
+  /     |
+_______/|\_
+''',
+    r'''
    +----+
    |    |
    o    |
@@ -16,18 +64,17 @@ FINAL_FIELD = r'''
   / \   |
 _______/|\_
 '''
-
-# здесь мы наверное хотим иметь исходное поле
-# и понимание, как оно меняется после каждого хода
-FIELD = FINAL_FIELD
+]  # Все возможные состояния игры
 
 while True:
     # make a move!
+    print(fields[mistakes])
+    print(guess_field)
     letter = input('Enter your guess: ')
     if ...:
         FIELD = ...  # если не угадали, то надо обновить поле
     else:
         ...  # мало ли, понадобится...
-
     clear_terminal()
-    print(FIELD)
+
+    # print(FIELD)
