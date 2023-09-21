9ae5aaceedbc0ab7ec6577400d11c13372a6b7e0
Diana Gindullina <72610827+Arti-D@users.noreply.github.com>
2023-09-22 01:49:23 +0300

    Блок hangman: - подключена библиотека рандомайзера слов - реализована функция Начать игру сначала - создание некоторых глобальных переменных вынесено в отдельную функцию - рефакторинг

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,12 +1,7 @@
 from common.util import clear_terminal
+from random_word import RandomWords #библиотека рандомных слов
 
-
-def create_secret():
-    return 'capybara'
-
-
-SECRET = create_secret()
-n = len(SECRET)
+randomWords = RandomWords()
 
 FINAL_FIELD = r'''
    +----+
@@ -15,19 +10,81 @@ FINAL_FIELD = r'''
   /|\   |
   / \   |
 _______/|\_
-'''
+'''.split('\n')
 
-# здесь мы наверное хотим иметь исходное поле
-# и понимание, как оно меняется после каждого хода
-FIELD = FINAL_FIELD
+HUMAN = [
+    (3, 3),
+    (4, 3),
+    (4, 2),
+    (4, 4),
+    (5, 2),
+    (5, 4)
+]
+human_parts = 0
 
-while True:
-    # make a move!
-    letter = input('Enter your guess: ')
-    if ...:
-        FIELD = ...  # если не угадали, то надо обновить поле
-    else:
-        ...  # мало ли, понадобится...
 
+def create_secret_word():
+    return randomWords.get_random_word()
+
+
+def global_variables():
+    secret = create_secret_word()
+    n = len(secret)
+    guessed = ['_' for i in range(n)]
+    field = [list(row) for row in FINAL_FIELD]
+    for cell in HUMAN:
+        field[cell[0]][cell[1]] = ' '
+    return secret, guessed, field, n
+
+
+SECRET, GUESSED, FIELD, n = global_variables()
+
+
+def play_again():
+    global SECRET, GUESSED, FIELD, n, human_parts
+    while True:
+        play_again = input('Want to play again? enter yes or no ').lower()
+        if play_again == 'yes':
+            SECRET, GUESSED, FIELD, n = global_variables()
+            human_parts = 0
+            return True
+        elif play_again == 'no':
+            return False
+        else:
+            print('Invalid enter. Please try again')
+
+
+FLAG = True
+while FLAG:
+    # 1
     clear_terminal()
-    print(FIELD)
+    for row in FIELD:
+        print(''.join(row))
+    print()
+    print(''.join(GUESSED))
+
+    # 2 and 3
+    letter = input('Enter your guess: ').lower()
+    if len(letter) != 1 and ord(letter) < ord('a') or ord(letter) > ord('z'):
+        print('Try again. Enter a letter')
+        continue
+    # 4
+    if letter in SECRET:
+        print('Great job!')
+        for i in range(n):
+            if SECRET[i] == letter:
+                GUESSED[i] = letter
+    else:
+        print('What a pity :(')
+        cell = HUMAN[human_parts]
+        human_parts += 1
+        FIELD[cell[0]][cell[1]] = FINAL_FIELD[cell[0]][cell[1]]
+
+    # 5
+    if '_' not in GUESSED:
+        print('You won!')
+        FLAG, SECRET, FIELD = play_again()
+
+    if human_parts == len(HUMAN):
+        print('You lose!')
+        FLAG = play_again()
