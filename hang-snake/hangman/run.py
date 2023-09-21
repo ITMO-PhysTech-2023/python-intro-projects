1c1e93e2482f124bfd7053b88b4c136543e7c704
PolichPon <ponopolya@gmail.com>
2023-09-22 00:45:20 +0300

    dictionary redrawing of field lose/win check game logic

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,33 +1,89 @@
 from common.util import clear_terminal
+from random import randint
 
 
 def create_secret():
-    return 'capybara'
+    dictionary = ['capybara', 'physics', 'polina']
+    return dictionary[randint(0, 2)]
+def check_letter(letter, secret, answer):
+    for i in range (len(secret)):
+        if (letter == secret[i]):
+            answer[i] = secret[i]
+    return answer
 
-
-SECRET = create_secret()
+SECRET = list(create_secret())
 n = len(SECRET)
 
-FINAL_FIELD = r'''
+FIELD = [r'''
+   +----+
+   |    |
+        |
+        |
+        |
+_______/|\_
+''', r'''
+   +----+
+   |    |
+   o    |
+        |
+        |
+_______/|\_
+''', r'''
+   +----+
+   |    |
+   o    |
+   |    |
+        |
+_______/|\_
+''', r'''
+   +----+
+   |    |
+   o    |
+  /|    |
+        |
+_______/|\_
+''', r'''
+   +----+
+   |    |
+   o    |
+  /|\   |
+        |
+_______/|\_
+''', r'''
+   +----+
+   |    |
+   o    |
+  /|\   |
+  /     |
+_______/|\_
+''',  r'''
    +----+
    |    |
    o    |
   /|\   |
   / \   |
 _______/|\_
-'''
+''']
 
-# здесь мы наверное хотим иметь исходное поле
-# и понимание, как оно меняется после каждого хода
-FIELD = FINAL_FIELD
+answer = list('_' * n)
+conditions = 0
+print(FIELD[0])
 
 while True:
-    # make a move!
+    print( *answer )
     letter = input('Enter your guess: ')
-    if ...:
-        FIELD = ...  # если не угадали, то надо обновить поле
-    else:
-        ...  # мало ли, понадобится...
+    new_answer = check_letter( letter, SECRET, answer.copy() )
+    #print(*answer, *new_answer)
+    if( new_answer == answer ):
+        conditions+=1
+
+    answer = new_answer
 
     clear_terminal()
-    print(FIELD)
+    print( FIELD[conditions] )
+    if( conditions == 6 ):
+        print( 'YOU LOSE' )
+        break
+    if( answer == SECRET):
+        print( 'TUTURU' )
+        break
