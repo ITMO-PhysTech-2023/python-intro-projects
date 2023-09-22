dbc25dce4d237fe561275eec65af093d9c8be1b2
NikitaAlexeev <nikitaalexeev17@mail.ru>
2023-09-22 20:51:02 +0300

    Added gitignore

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -5,9 +5,13 @@ def create_secret():
     return 'capybara'
 
 
+def vivod(x): # функция, кторая выводит field по строкам
+    for i in range(len(x)):
+        print(''.join(x[i]))
+
 SECRET = create_secret()
 n = len(SECRET)
-
+GUESSED = ['_' for _ in range(n)]
 FINAL_FIELD = r'''
    +----+
    |    |
@@ -15,19 +19,44 @@ FINAL_FIELD = r'''
   /|\   |
   / \   |
 _______/|\_
-'''
-
-# здесь мы наверное хотим иметь исходное поле
-# и понимание, как оно меняется после каждого хода
+'''.split('\n')
 FIELD = FINAL_FIELD
-
+HUMAN = [
+    (3, 3),
+    (4, 3),
+    (4, 2),
+    (4, 4),
+    (5, 2),
+    (5, 4)
+]
+FIELD = [
+    list(row)
+    for row in FINAL_FIELD
+]
+for cell in HUMAN:
+    FIELD[cell[0]][cell[1]] = ' '
+human_parts = 0
+ch=0 # индекс части тела
+chasti_tela=['o','|','/','\\','/','\\']
 while True:
     # make a move!
     letter = input('Enter your guess: ')
-    if ...:
-        FIELD = ...  # если не угадали, то надо обновить поле
-    else:
-        ...  # мало ли, понадобится...
+    if letter not in SECRET:
+        a, b = HUMAN[human_parts]
+        FIELD[a][b] = chasti_tela[ch]
+        human_parts += 1
+        ch += 1
+        vivod(FIELD)
+        print(GUESSED)
+        if human_parts == 6:
+            print('YOU LOSE')
+            break
 
-    clear_terminal()
-    print(FIELD)
+    else:
+        for i in range(n):
+            if SECRET[i] == letter:
+                GUESSED[i] = letter
+        print(GUESSED)
+    if '_' not in GUESSED:
+        print('You won!')
+        break
---
+++ hang-snake/snake/snake_pygame.py
@@ -0,0 +1,53 @@
+import pygame
+from random import randrange
+SIZE=800# размеры поля
+SIZE_body=50# шаг змеи
+x,y=randrange(0,SIZE,SIZE_body),randrange(0,SIZE,SIZE_body)
+apple=randrange(0,SIZE,SIZE_body),randrange(0,SIZE,SIZE_body)
+l=1
+snake=[(x, y)] # список координат
+x1,y1=0,0
+fps=10
+
+pygame.init()
+sc=pygame.display.set_mode([SIZE, SIZE])
+clock=pygame.time.Clock()
+
+while True:
+    sc.fill(pygame.Color('black'))# перед каждым след действием красит поле игры в черный
+    [(pygame.draw.rect(sc,pygame.Color('green'),(i,j,SIZE_body,SIZE_body))) for i,j in snake]
+    pygame.draw.rect(sc,pygame.Color('red'),(*apple,SIZE_body,SIZE_body))
+    x+=x1*SIZE_body# движение змеи
+    y+=y1*SIZE_body# точнее: координаты увеличиваются на размер ее головы
+    snake.append((x,y))
+    snake=snake[-l:]
+    # поедание яблока
+    if snake[-1]==apple:# поедание яблока
+        apple=randrange(0,SIZE,SIZE_body),randrange(0,SIZE,SIZE_body)
+        l+=1
+        fps+=1
+    # конец игры
+    if x<0 or x>SIZE-SIZE_body or y<0 or y>SIZE-SIZE_body:# условия выхода из игры(если координаты по х и у выходят за пределы(берем - SIZE_body , так как змея может выйти на длину головы, чтобы повернуть)
+        break
+    if len(snake)!=len(set(snake)):# проверка на самопоедание ( в сете могут быть повторяющиеся координаты => змея уперлась в себя)
+        break
+
+
+
+    pygame.display.flip()
+    clock.tick(fps)
+
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            exit()
+
+    # управление
+    key=pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        x1,y1=0,-1
+    if key[pygame.K_s]:
+        x1,y1=0,1
+    if key[pygame.K_a]:
+        x1,y1=-1,0
+    if key[pygame.K_d]:
+        x1,y1=1,0
