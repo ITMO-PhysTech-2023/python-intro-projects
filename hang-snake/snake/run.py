315161a8934c71bdb742ac6f6a515f769ebefdb7
dasha <dashatoropona01112005@gmail.com>
2023-09-23 19:21:26 +0300

    Готовая рабочая виселицаа

--- hang-snake/snake/run.py
+++ hang-snake/snake/run.py
@@ -1,13 +1,24 @@
 from pynput import keyboard
 from random import randint
 
-WIDTH, HEIGHT = 20, 20
+#WIDTH, HEIGHT = 20,20
+class SnakeGame:
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+        self.direction=(1,0)
+        #self.snake=Snake(...)
+
+Snake = SnakeGame(width=int(input()), height=int(input()))
+
 # можно приделать конфиг-файл с параметрами
-direction = (1, 0)
+#direction = (1, 0)
 
 
 def random_position():
-    return randint(0, HEIGHT - 1), randint(0, WIDTH - 1)
+    position = []
+    position = [randint(0, Snake.height - 1), randint(0, Snake.width - 1)]
+    return position
 
 
 def process_press(key):
@@ -24,13 +35,21 @@ def process_press(key):
             direction = (1, 0)
 
 
-snake = [random_position()]
-apple = random_position()
-while apple in snake:
-    apple = random_position()
 
-# оно умеет мониторить нажатия на кнопки!
+snake = random_position()
+apple = random_position()
+print(*snake,*apple)
+
+# while apple[0] == snake[0]:
+#     new_apple = [random_position()]
+#     apple=new_apple
+
+# # оно умеет мониторить нажатия на кнопки!
 with keyboard.Listener(on_press=process_press) as listener:
     while True:
-        # let's play the game!
-        pass
+        if (int(snake[0]) == Snake.height or int(snake[0]) == -1 or int(snake[1]) == -1 or int(snake[1]) == Snake.width):
+            break
+        else:
+            if (apple[1] == snake[1] and apple[0] == snake[0]):
+                new_apple = random_position()
+                apple = new_apple
