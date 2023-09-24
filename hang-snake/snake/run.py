98a5e6443a3be6b1a26fcfbabb6f1a7d00a0c156
100yef <100yef@gmail.com>
2023-09-24 17:30:23 +0300

    Snake + UI version pre-alpha (window added)

--- hang-snake/snake/run.py
+++ hang-snake/snake/run.py
@@ -1,27 +1,17 @@
 from pynput import keyboard
 from random import randint
+from tkinter import *
 
-WIDTH, HEIGHT = ..., ...
-# можно приделать конфиг-файл с параметрами
-direction = (1, 0)
+WIDTH = 700
+HEIGHT = 700
+OBJ_SIDE = 35
+GAME_SPEED = 100
+direction = 'right'
+
 
 
 def random_position():
     return randint(0, HEIGHT - 1), randint(0, WIDTH - 1)
-
-
-def process_press(key):
-    # обработчик нажатия на клавиши (можно сделать и поаккуратнее)
-    global direction
-    match key:
-        case keyboard.Key.left:
-            direction = (0, -1)
-        case keyboard.Key.up:
-            direction = (-1, 0)
-        case keyboard.Key.right:
-            direction = (0, 1)
-        case keyboard.Key.down:
-            direction = (1, 0)
 
 
 snake = [random_position()]
@@ -29,8 +19,11 @@ apple = random_position()
 while apple in snake:
     apple = random_position()
 
-# оно умеет мониторить нажатия на кнопки!
-with keyboard.Listener(on_press=process_press) as listener:
-    while True:
-        # let's play the game!
-        pass
+window = Tk()
+window.title('Sneik')
+
+canvas = Canvas(window, bg="black", height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+window.geometry(f"{700}x{700}+{400}+{100}")
+window.mainloop()
