1203fa5336534a66dc173594b5a52cc5dabed470
100yef <100yef@gmail.com>
2023-09-24 17:41:00 +0300

    Snake + UI version beta (movement added)

--- hang-snake/snake/run.py
+++ hang-snake/snake/run.py
@@ -29,6 +29,44 @@ class Apple:
         canvas.create_rectangle(self.x, self.y, self.x + OBJ_SIDE, self.y + OBJ_SIDE, fill="red", tags="apple")
 
 
+def move(snake, apple):
+    x, y = snake.coords[0]
+    if direction == 'left':
+        x = x - OBJ_SIDE
+    elif direction == 'right':
+        x = x + OBJ_SIDE
+    elif direction == 'up':
+        y = y - OBJ_SIDE
+    elif direction == 'down':
+        y = y + OBJ_SIDE
+
+    snake.coords.insert(0, (x, y))
+    part = canvas.create_rectangle(x, y, x + OBJ_SIDE, y + OBJ_SIDE, fill="green")
+    snake.parts.insert(0, part)
+
+
+    if x == apple.coords[0] and y == apple.coords[1]:
+        canvas.delete("apple")
+        apple = Apple()
+    else:
+        del snake.coords[-1]
+        canvas.delete(snake.parts[-1])
+        del snake.parts[-1]
+
+    window.after(GAME_SPEED, move, snake,apple)
+
+def change_direction(new_direction):
+    global direction
+    match new_direction:
+        case 'left':
+            direction = 'left'
+        case 'right':
+            direction = 'right'
+        case 'up':
+            direction = 'up'
+        case 'down':
+            direction = 'down'
+
 def random_position():
     return randint(0, HEIGHT - 1), randint(0, WIDTH - 1)
 
@@ -44,4 +82,12 @@ apple = Apple()
 
 
 window.geometry(f"{700}x{700}+{400}+{100}")
+
+move(snake, apple)
+
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+
 window.mainloop()
