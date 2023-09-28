a9711364fa6ef6acee266ca7e5e5aa6c8c5801fb
PYanix <140638821+PYanix@users.noreply.github.com>
2023-09-29 00:04:11 +0300

    первая хоть немного работающая версия

--- hang-snake/snake/run.py
+++ hang-snake/snake/run.py
@@ -1,9 +1,50 @@
 from pynput import keyboard
 from random import randint
+import os
+import threading
+mutex = threading.Lock()
+def spawnApple():
+    while True:
+        tryApple = random_position()
+        if tryApple not in snake:
+            return tryApple
+def printField():
+    os.system('cls')
+    for i in range(WIDTH):
+        for j in range(HEIGHT):
+            if (i, j) in snake:
+                print(0, end=' ')
+            elif (i,j) == apple:
+                print('A', end=' ')
+            else:
+                print('_', end=' ')
+        print()
 
-WIDTH, HEIGHT = ..., ...
-# можно приделать конфиг-файл с параметрами
-direction = (1, 0)
+def scrawl():
+    threading.Timer(1.0, scrawl).start()
+
+    x = snake[0][0]+direction[0]
+    y = snake[0][1] + direction[1]
+    snake.insert(0,(x,y))
+    if deleteLastCoor == True:
+        snake.pop()
+    if keepMoving()==False:
+        print("Game over")
+        threading.join()
+    printField()
+
+
+def keepMoving():
+    global apple
+    if snake[0] == apple:
+        global deleteLastCoor
+        deleteLastCoor = False
+        apple = spawnApple()
+    else:
+        deleteLastCoor = True
+    if (len(snake)>1 and snake[0] in snake[1:]) or snake[0][0]==-1 or snake[0][1]==-1 or snake[0][0]==HEIGHT or snake[0][1]==WIDTH:
+        return False
+    return True
 
 
 def random_position():
@@ -24,13 +65,15 @@ def process_press(key):
             direction = (1, 0)
 
 
+WIDTH, HEIGHT = 10, 10
+# можно приделать конфиг-файл с параметрами
+direction = (1, 0)
+deleteLastCoor = True
 snake = [random_position()]
 apple = random_position()
-while apple in snake:
-    apple = random_position()
-
+printField()
+scrawl()
 # оно умеет мониторить нажатия на кнопки!
 with keyboard.Listener(on_press=process_press) as listener:
-    while True:
-        # let's play the game!
-        pass
+    listener.join()
+
