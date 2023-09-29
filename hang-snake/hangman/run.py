ec5ecf62c57b7b43b308b47578300e97d795551a
lizavetashap <liza-shap@yandex.ru>
2023-09-29 13:05:36 +0300

    проверка сохранение промежуточного

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,101 +1,11 @@
 from common.util import clear_terminal
-
+from FIELD import FIELD
 
 
 def create_secret():
     return 'солнышко'
-word = create_secret()
 
-FIELD = (
-    """
-              
-              
-              
-              
-              
-     _______/|\_ 
-    """,
-    """
-              
-              
-              
-             |
-             |
-     _______/|\_ 
-    """,
-    """
-              
-             |
-             |
-             |
-             |
-     _______/|\_ 
-    """,
-    """
-       +-----+
-             |
-             |
-             |
-             |
-     _______/|\_ 
-    """,
-    """
-       +-----+
-       |     |
-             |
-             |
-             |
-     _______/|\_ 
-    """,
-    """
-       +-----+ 
-       |     |
-       o     |
-             |
-             |
-     _______/|\_ 
-    """,
-    """
-       +-----+
-       |     |
-       o     |
-       |     |
-             |
-     _______/|\_ 
-    """,
-    """
-       +-----+
-       |     |
-       o     |
-      /|     |
-             |
-     _______/|\_ 
-    """,
-    """
-       +-----+
-       |     |
-       o     |
-      /|\    |
-             |
-     _______/|\_ 
-    """,
-    """
-       +-----+
-       |     |
-       o     |
-      /|\    |
-      /      |
-     _______/|\_ 
-    """,
-    """
-       +-----+
-       |     |
-       o     |
-      /|\    |
-      / \    |
-     _______/|\_ 
-    """
-)
+word = create_secret()
 number_of_errors = 0 #кол-во ошибок сделанных игроком
 maximum_erorrs = 10 #максимальное количество ошибок
 n = len(word)
@@ -103,11 +13,12 @@ guessed = ['_' for _ in range(n)] #угаданные
 print(''.join(guessed))
 
 while guessed != word and number_of_errors < maximum_erorrs:
-    print('Enter your guess: ')# make a move!
-    letter = input() #игрок вводит букву
+    clear_terminal()
+    print('Enter your guess: ')  # make a move!
+    letter = input()  # игрок вводит букву
     if letter in word:
         new = ""
-        for i in range(len(word)): #ищем место буквы в слове
+        for i in range(len(word)):  # ищем место буквы в слове
             if letter == word[i]:
                 new += letter
             else:
@@ -117,7 +28,7 @@ while guessed != word and number_of_errors < maximum_erorrs:
         print(FIELD[number_of_errors])
     else:
         print(('There is no such letter in word.'))
-        number_of_errors +=1
+        number_of_errors += 1
         print(''.join(guessed))
         print(FIELD[number_of_errors])
 
