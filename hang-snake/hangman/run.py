d2d957ca23c4a941790b8f1cc907b7816d9e6e63
Александр <nikol.alex06@mail.ru>
2023-09-29 11:52:31 +0300

    Готовые версии игр

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,33 +1,113 @@
 from common.util import clear_terminal
+from pynput import keyboard
+
+a = 0
+def repeat(key):
+    global a
+    if key == keyboard.Key.space:
+        a = 'continue'
+    else:
+        a = 'break'
 
 
-def create_secret():
-    return 'capybara'
+secret_word = input('enter secret word: ').lower()
 
-
-SECRET = create_secret()
-n = len(SECRET)
-
-FINAL_FIELD = r'''
+lose_field = r'''
    +----+
    |    |
    o    |
   /|\   |
   / \   |
 _______/|\_
-'''
+'''.split('\n')
+loser = [
+    (3, 3),
+    (4, 3),
+    (4, 2),
+    (4, 4),
+    (5, 2),
+    (5, 4)
+]
+last_letter = []
+picture = [list(row) for row in lose_field]
+for i in loser:
+    picture[i[0]][i[1]] = ' '
 
-# здесь мы наверное хотим иметь исходное поле
-# и понимание, как оно меняется после каждого хода
-FIELD = FINAL_FIELD
+part_player = 0
+
+n = len(secret_word)
+
+enter_field = ['_' for _ in range(n)]
 
 while True:
-    # make a move!
-    letter = input('Enter your guess: ')
-    if ...:
-        FIELD = ...  # если не угадали, то надо обновить поле
-    else:
-        ...  # мало ли, понадобится...
-
     clear_terminal()
-    print(FIELD)
+    for row in picture:
+        print(''.join(row))
+    print()
+    print()
+    print(''.join(enter_field))
+    letter = input('enter letter: ').lower()
+    if ord(letter) < ord('a') or ord(letter) > ord('z') and len(letter) != 1:
+        print('Error of the enter')
+        continue
+    elif letter in last_letter:
+        print('This letter was already entered. Try other.')
+        continue
+    if letter in secret_word:
+        for i in range(n):
+            if secret_word[i] == letter:
+                enter_field[i] = letter
+    else:
+        part = loser[part_player]
+        picture[part[0]][part[1]] = lose_field[part[0]][part[1]]
+        part_player += 1
+
+    last_letter.append(letter)
+
+    if len(loser) == part_player:
+        print('You lose! Congratulations!')
+        print()
+        print('If you want to try again press to space. If you want to stop press other')
+        '''try:
+            if keyboard.is_pressed('o'):
+                while True:
+                    secret_word = input('enter secret word: ').lower()
+                    for i in secret_word:
+                        if ord(i) < ord('a') or ord(i) > ord('z'):
+                            print('A mistake in the word')
+                            continue
+                    break
+                last_letter = []
+                for i in loser:
+                    picture[i[0]][i[1]] = ' '
+                part_player = 0
+                n = len(secret_word)
+                enter_field = ['_' for _ in range(n)]
+                continue 
+        except:'''
+        break
+
+    elif '_' not in enter_field:
+        print('You won! Congratulations!')
+        print()
+        print(''.join(enter_field))
+        print()
+        print('If you want to try again press to space. If you want to stop press other')
+        '''try:
+            if keyboard.is_pressed('o'):
+                while True:
+                    secret_word = input('enter secret word: ').lower()
+                    for i in secret_word:
+                        if ord(i) < ord('a') or ord(i) > ord('z'):
+                            print('A mistake in the word')
+                            continue
+                    break
+                last_letter = []
+                for i in loser:
+                    picture[i[0]][i[1]] = ' '
+                part_player = 0
+                n = len(secret_word)
+                enter_field = ['_' for _ in range(n)]
+                continue
+        except:'''
+        break
--- hang-snake/snake/run.py
+++ hang-snake/snake/run.py
@@ -1,14 +1,15 @@
+import time
+
+from common.util import clear_terminal
 from pynput import keyboard
 from random import randint
 
-WIDTH, HEIGHT = ..., ...
-# можно приделать конфиг-файл с параметрами
+width, height = 20, 20
+
 direction = (1, 0)
 
-
-def random_position():
-    return randint(0, HEIGHT - 1), randint(0, WIDTH - 1)
-
+def generate_position():
+    return randint(0, width - 1), randint(0, height - 1)
 
 def process_press(key):
     # обработчик нажатия на клавиши (можно сделать и поаккуратнее)
@@ -23,14 +24,42 @@ def process_press(key):
         case keyboard.Key.down:
             direction = (1, 0)
 
+snake = [generate_position()]
+apple = generate_position()
+body_snake = 0
 
-snake = [random_position()]
-apple = random_position()
 while apple in snake:
-    apple = random_position()
+    apple = generate_position()
 
-# оно умеет мониторить нажатия на кнопки!
 with keyboard.Listener(on_press=process_press) as listener:
     while True:
-        # let's play the game!
-        pass
+        clear_terminal()
+        # рисуем змею и яблоко на поле
+        field = [['.' for i in range(width)] for i in range(height)]
+        new_elem = (snake[0][0] + direction[0], snake[0][1] + direction[1])
+        if body_snake == 0:
+            field[snake[0][0]][snake[0][1]] = 'O'
+        else:
+            for i in range(body_snake + 1):
+                if i == 0:
+                    field[snake[0][0]][snake[0][1]] = 'O'
+                else:
+                    field[snake[i][0]][snake[i][1]] = 'o'
+        field[apple[0]][apple[1]] = 'a'
+
+        # выводим картинку
+
+        for row in field:
+            print(' '.join(row))
+
+        # передвигаем змею
+
+        snake.insert(0, new_elem)
+
+        if new_elem == apple:
+            while apple in snake:
+                apple = generate_position()
+            body_snake += 1
+        else:
+            snake.pop(-1)
+        time.sleep(0.3)
