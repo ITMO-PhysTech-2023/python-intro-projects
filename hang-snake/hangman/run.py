61e4e510426b46b565973a00dc883cf560752cf8
lizavetashap <liza-shap@yandex.ru>
2023-10-05 23:39:30 +0300

    snake-hangman змейка и виселица обернуты в классы. виселица работает от генератора букв в файле common в змейку генерируется буква из common и пока змея ее не съест - буква не поменяется как "сделать поле в виде таблицы символов, и потом просто пройтись по змейке и для каждого ее элемента, поставить нужный символ в соответствующую ячейку" - пока не поняла, надеюсь поможете на практике переменную eat не внесла в функцию, потому что она используется не только в ней, поэтому нам нужно чтобы она была в __init__ (насколько я поняла ваш комментарий) остальное вроде сделала

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,42 +1,84 @@
 from common.util import clear_terminal
-from FIELD import FIELD
+import time
+
+from common.run import LetterProvider, RandomLetterProvider
 
 
 def create_secret():
-    return 'солнышко'
+    return 'capybara'
 
-word = create_secret()
-number_of_errors = 0 #кол-во ошибок сделанных игроком
-MAX_ERRORS = 10 #максимальное количество ошибок
-n = len(word)
-guessed = ['_' for _ in range(n)] #угаданные
-print(''.join(guessed))
 
-while guessed != word and number_of_errors < MAX_ERRORS:
-    clear_terminal()
-    print('Enter your guess: ')  # make a move!
-    letter = input()  # игрок вводит букву
-    if letter in word:
-        new = ""
-        for i in range(len(word)):  # ищем место буквы в слове
-            if letter == word[i]:
-                new += letter
-            else:
-                new += guessed[i]
-        guessed = new
-        print(''.join(guessed))
-        print(FIELD[number_of_errors])
-    else:
-        print(('There is no such letter in word.'))
-        number_of_errors += 1
-        print(''.join(guessed))
-        print(FIELD[number_of_errors])
+class Hangmangame:
+    def __init__(self, letter_provider: LetterProvider, step_sleep: int):
+        self.word = create_secret()
+        self.n = len(self.word)
+        self.guessed = ['_' for _ in range(self.n)]
+        self.provider = letter_provider
+        from FIELD import FIELD
+        self.field = FIELD
+        self.step_sleep = step_sleep
+        self.number_of_errors = 0
+        self.MAX_ERRORS = 10
 
-if number_of_errors == MAX_ERRORS:
-    print(FIELD[number_of_errors])
-    print("Game over")
-else:
-    print("Congratulations!The word was", word)
+    def read_guess(self) -> str:
+        while True:
+            letter = self.provider.get_next_letter().lower()
+            if len(letter) != 1 and ord(letter) < ord('a') or ord(letter) > ord('z'):
+                print('Invalid guess! Try again (enter a letter)')
+                continue
+            return letter
 
-    clear_terminal()
+    def check_guess(self, letter: str):
+        if letter in self.word:
+            new = ""
+            for i in range(len(self.word)):  # ищем место буквы в слове
+                if letter == self.word[i]:
+                    new += letter
+                else:
+                    new += self.guessed[i]
+            self.guessed = new
+        else:
+            print('There is no such letter in word.')
+            self.number_of_errors += 1
 
+    def check_possibility(self):
+        while True:
+            if self.guessed != self.word and\
+               self.number_of_errors < self.MAX_ERRORS:
+                return
+
+    def is_won(self) -> bool:
+        return self.guessed == self.word
+
+    def is_lost(self) -> bool:
+        return self.MAX_ERRORS == self.number_of_errors
+
+    def step(self):
+        letter = self.read_guess()
+        self.check_guess(letter)
+        time.sleep(self.step_sleep)
+
+    def show(self):
+        clear_terminal()
+        print(''.join(self.guessed))
+        print(self.field[self.number_of_errors])
+
+    def run(self):
+        self.show()
+        while True:
+            self.check_possibility()
+            self.step()
+            if self.is_won():
+                self.show()
+                print("Congratulations!The word was", self.word)
+                break
+            if self.is_lost():
+                self.show()
+                print("Game over")
+                break
+            self.show()
+
+
+provider = RandomLetterProvider()
+game = Hangmangame(provider, 1)
+game.run()
--- hang-snake/snake/run.py
+++ hang-snake/snake/run.py
@@ -2,98 +2,92 @@ from pynput import keyboard
 import time
 import os
 from random import randint
+from common.run import LetterProvider, RandomLetterProvider
 
 
-# генерируем позицию яблока
-def random_position():
-    row = randint(1, HEIGHT - 2)
-    col = randint(1, WIDTH - 2)
-    while (col, row) in snake:  # проверяем не в змею ли сгенерировалось
-        row = randint(1, HEIGHT - 2)
-        col = randint(1, WIDTH - 2)
-    return(col, row)
+class Snakegame:
+    def __init__(self, letter_provider: LetterProvider):
+        self.WIDTH = 30
+        self.HEIGHT = 15
+        self.CELLS = [(col, row) for row in range(self.HEIGHT) for col in range(self.WIDTH)]
+        self.snake = [(5, self.HEIGHT // 2), (4, self.HEIGHT // 2), (3, self.HEIGHT // 2)]
+        self.apple = self.random_position()
+        self.eat = False
+        self.flag = False
+        self.direction = (1, 0)
+        self.provider = letter_provider
+        self.letter = 'a'
 
+    def random_position(self):
+        row = randint(1, self.HEIGHT - 2)
+        col = randint(1, self.WIDTH - 2)
+        while (col, row) in self.snake:
+            row = randint(1, self.HEIGHT - 2)
+            col = randint(1, self.WIDTH - 2)
+        return col, row
 
-# наше поле
-def print_field():
-    for cell in CELLS:
-        if cell in snake:
-            print('s', end = '')
-        elif cell[0] in (0, WIDTH - 1):
-            print('|', end = '')
-        elif cell[1] in (0, HEIGHT - 1):
-            print('_', end = '')
-        elif cell == apple:
-            print('a', end = '')
+    def apple_random(self):
+        if self.apple == self.snake[0]:
+            self.apple = self.random_position()
+            self.eat = True
+            self.letter = self.provider.get_next_letter().lower()
+
+    def print_field(self):
+        for cell in self.CELLS:
+            if cell in self.snake:
+                print('s', end='')
+            elif cell[0] in (0, self.WIDTH - 1):
+                print('|', end='')
+            elif cell[1] in (0, self.HEIGHT - 1):
+                print('_', end='')
+            elif cell == self.apple:
+                print(self.letter, end='')
+            else:
+                print(' ', end='')
+
+            if cell[0] == self.WIDTH - 1:
+                print('')
+
+    def snake_move(self):
+        head_snake = self.snake[0][0] + self.direction[0], self.snake[0][1] + self.direction[1]
+        self.snake.insert(0, head_snake)
+        if not self.eat:
+            self.snake.pop()
         else:
-            print(' ', end = '')
+            self.eat = False
 
-        if cell[0] == WIDTH -1:
-            print('')
+    def process_press(self, key):
+        match key:
+            case keyboard.Key.left:
+                self.direction = (-1, 0)
+            case keyboard.Key.up:
+                self.direction = (0, -1)
+            case keyboard.Key.right:
+                self.direction = (1, 0)
+            case keyboard.Key.down:
+                self.direction = (0, 1)
+            case keyboard.Key.esc:
+                self.flag = True
 
+    def is_lost(self) -> bool:
+        return self.snake[0][0] in (0, self.WIDTH) or\
+                self.snake[0][1] in (0, self.HEIGHT) or\
+                self.snake[0] in self.snake[1:] or self.flag
 
-# змейка ползает
-def snake_move():
-    global eat
-    head_snake = snake[0][0] + direction[0], snake[0][1] + direction[1]
-    # делаем змейке новую голову, чтобы она ползла вперед
-    snake.insert(0, head_snake)
-    if not eat: # если змея не съела яблоко то удаляем хвост
-        snake.pop()
-    else:
-        eat = False
-
-
-# механизм поедания яблока
-def apple_random():
-    global apple, eat
-    if apple == snake[0]:
-        apple = random_position()
-        eat = True
-
-WIDTH, HEIGHT = 30, 15  # параметры поля
-CELLS = [(col, row) for row in range(HEIGHT) for col in range(WIDTH)]
-
-snake = [(5, HEIGHT // 2), (4, HEIGHT // 2), (3, HEIGHT // 2)]  # генерируем змею на поле
-apple = random_position()  # генерируем яблоко на поле
-eat = False  # проверка съедено ли яблоко
-flag = False  # завели отдельную метку для того, чтобы выключать змейку
-
-
-# управление
-def process_press(key):
-    # обработчик нажатия на клавиши (можно сделать и поаккуратнее)
-    global direction, flag
-    match key:
-        case keyboard.Key.left:
-            direction = (-1, 0)
-        case keyboard.Key.up:
-            direction = (0, -1)
-        case keyboard.Key.right:
-            direction = (1, 0)
-        case keyboard.Key.down:
-            direction = (0, 1)
-        case keyboard.Key.esc:
-            flag = True
+    def run(self):
+        with keyboard.Listener(on_press=self.process_press) as listener:
+            while True:
+                os.system('cls')
+                self.print_field()
+                self.apple_random()
+                self.snake_move()
+                if self.is_lost():
+                    os.system('cls')
+                    print('Game Over')
+                    break
+                time.sleep(0.3)
 
 
-direction = (1, 0)
-
-# делаем больше одного хода
-with keyboard.Listener(on_press=process_press) as listener:
-    while True:
-        os.system('cls')
-        print_field()
-        apple_random()
-        snake_move()
-
-        # проверяем не врезалась ли змейка в границы и в себя
-        if snake[0][0] in (0, WIDTH) or\
-            snake[0][1] in (0, HEIGHT) or\
-            snake[0] in snake[1:] or\
-            flag:  # оно умеет выключаться при нажатии Esc
-            os.system('cls')
-            print('Game Over')
-            break
-
-        time.sleep(0.3)
+provider = RandomLetterProvider()
+game = Snakegame(provider)
+game.run()
